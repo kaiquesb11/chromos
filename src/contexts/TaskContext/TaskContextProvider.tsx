6351bec9a3113d13ba9401e7e784dd521b0b1f9a
Kaique Barbosa <kaiquebarbosa@MacBook-Pro-de-Kaique.local>
2026-01-01 16:36:25 -0400

    mudando useSatate para useReducer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,41 +1,22 @@
-import { useEffect, useReducer, useState } from "react";
-import { initialTaskState } from "./initialTaskState";
-import { TaskContext } from "./TaskContext";
+import { useEffect, useReducer } from 'react';
+import { initialTaskState } from './initialTaskState';
+import { TaskContext } from './TaskContext';
+import { taskReducer } from './taskReducer';
 
 type TaskContextProviderProps = {
     children: React.ReactNode;
 };
 
-export function TaskContextProvider({children}: TaskContextProviderProps) {
-    const [state, setState] = useState(initialTaskState);
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+    const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
-    const [numero, dispatch] = useReducer((state, action) => {
-        console.log (state, action);
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
 
-        switch (action){
-            case "INCREMENTE":
-                return state + 1;
-            
-            case "DECREMENTAR":
-                return state - 1;
-
-            case "ZERAR":
-                return 0;
-        }
-
-        return state;
-    }, 0)
-
-    // useEffect(() => {
-    //     console.log(state);
-    // }, [state]);
-
-    return(
-        <TaskContext.Provider value= {{state, setState}}>
-            <h1>O número é: {numero}</h1>
-            <button onClick={() => dispatch("INCREMENTE")}>Incrementar</button>
-            <button onClick={() => dispatch("DECREMENTAR")}>Decrementar</button>
-            <button onClick={() => dispatch("ZERAR")}>Zerar</button>
+    return (
+        <TaskContext.Provider value={{ state, dispatch }}>
+        {children}
         </TaskContext.Provider>
     );
 }
